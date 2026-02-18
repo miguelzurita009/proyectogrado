fa2dcb96a656cd01332a035ce76d8f9bc529afbf
Miguel <miguelzurita009@gmail.com>
2026-02-17 20:12:08 -0400

    ruteo y login sin testear

---
+++ vistas/modulos/login.php
@@ -0,0 +1,115 @@
+<main class="auth-cover-wrapper">
+  <div class="auth-cover-content-inner">
+    <div class="auth-cover-content-wrapper">
+      <div class="auth-img">
+        <img
+          src="vistas/assets/images/auth/auth-cover-login-bg.svg"
+          alt=""
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
+  <div class="auth-cover-sidebar-inner">
+    <div class="auth-cover-card-wrapper">
+      <div class="auth-cover-card p-sm-5">
+        <div class="wd-50 mb-5">
+          <img src="vistas/assets/images/logo-abbr.png" alt="" class="img-fluid" />
+        </div>
+        <h2 class="fs-20 fw-bolder mb-4">Login</h2>
+        <h4 class="fs-13 fw-bold mb-2">Login to your account</h4>
+        <p class="fs-12 fw-medium text-muted">
+          Thank you for get back <strong>Nelel</strong> web applications, let's
+          access our the best recommendation for you.
+        </p>
+        <form action="index.html" class="w-100 mt-4 pt-2">
+          <div class="mb-4">
+            <input
+              type="email"
+              class="form-control"
+              placeholder="Email or Username"
+              value="wrapcode.info@gmail.com"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <input
+              type="password"
+              class="form-control"
+              placeholder="Password"
+              value="123456"
+              required
+            />
+          </div>
+          <div class="d-flex align-items-center justify-content-between">
+            <div>
+              <div class="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  class="custom-control-input"
+                  id="rememberMe"
+                />
+                <label class="custom-control-label c-pointer" for="rememberMe"
+                  >Remember Me</label
+                >
+              </div>
+            </div>
+            <div>
+              <a href="auth-reset-cover.html" class="fs-11 text-primary"
+                >Forget password?</a
+              >
+            </div>
+          </div>
+          <div class="mt-5">
+            <button type="submit" class="btn btn-lg btn-primary w-100">
+              Login
+            </button>
+          </div>
+        </form>
+        <div class="w-100 mt-5 text-center mx-auto">
+          <div class="mb-4 border-bottom position-relative">
+            <span
+              class="small py-1 px-3 text-uppercase text-muted bg-white position-absolute translate-middle"
+              >or</span
+            >
+          </div>
+          <div class="d-flex align-items-center justify-content-center gap-2">
+            <a
+              href="javascript:void(0);"
+              class="btn btn-light-brand flex-fill"
+              data-bs-toggle="tooltip"
+              data-bs-trigger="hover"
+              title="Login with Facebook"
+            >
+              <i class="feather-facebook"></i>
+            </a>
+            <a
+              href="javascript:void(0);"
+              class="btn btn-light-brand flex-fill"
+              data-bs-toggle="tooltip"
+              data-bs-trigger="hover"
+              title="Login with Twitter"
+            >
+              <i class="feather-twitter"></i>
+            </a>
+            <a
+              href="javascript:void(0);"
+              class="btn btn-light-brand flex-fill"
+              data-bs-toggle="tooltip"
+              data-bs-trigger="hover"
+              title="Login with Github"
+            >
+              <i class="feather-github text"></i>
+            </a>
+          </div>
+        </div>
+        <div class="mt-5 text-muted">
+          <span> Don't have an account?</span>
+          <a href="auth-register-cover.html" class="fw-bold"
+            >Create an Account</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
